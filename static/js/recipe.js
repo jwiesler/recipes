00f27bf27edb65c37cee386df19252e68bc16b37
Julian Wiesler <wiesleju@gmail.com>
2020-08-21 17:24:34 +0200

    Show ingredients of different units in scaler

--- static/js/recipe.js
+++ static/js/recipe.js
@@ -18,7 +18,6 @@ function createIngredientInfo(name, unit) {
     return {
         name: name,
         unit: unit,
-        valid: true,
         amount: 0,
     }
 }
@@ -28,41 +27,39 @@ function scanIngredients(info, ingredientInfos, validRows) {
         extendSection(section)
         section.ingredients().forEach(function(row) {
             extendRow(row)
+
             const ingredient = row.ingredient
             const amount = ingredient.parseAmount()
             const amountValid = !isNaN(amount)
-            let ingredientInfo = ingredientInfos.get(ingredient.name)
+            if(!amountValid)
+                return
+
+            const key = makeIngredientKey(ingredient)
+            let ingredientInfo = ingredientInfos.get(key)
             if(ingredientInfo === undefined) {
                 ingredientInfo = createIngredientInfo(ingredient.name, ingredient.unit)
-                ingredientInfos.set(ingredient.name, ingredientInfo)
+                ingredientInfos.set(key, ingredientInfo)
             }
 
-            const isValidRow = amountValid && ingredient.unit === ingredientInfo.unit
-            ingredientInfo.valid = ingredientInfo.valid && isValidRow
-            if(isValidRow) {
-                ingredientInfo.amount += amount
-            }
-            if(amountValid) {
-                validRows.push(row)
-            }
+            ingredientInfo.amount += amount
+            validRows.push(row)
         })
     })
+}
 
-    const invalids = []
-    ingredientInfos.forEach(function(value, key) {
-        if(!value.valid)
-            invalids.push(key)
-    })
-
-    invalids.forEach(function(k) {
-        ingredientInfos.delete(k)
-    })
+function makeIngredientKey(ingredient) {
+    let text = ingredient.name
+    if(ingredient.unit) {
+        text += " (" + ingredient.unit + ")"
+    }
+    return text
 }
 
 $(function() {
     const ingredientInfos = new Map()
     const validRows = []
     const info = createInitialState()
+
     scanIngredients(info, ingredientInfos, validRows)
 
     function scaleAllByFactor(factor) {
@@ -82,6 +79,7 @@ $(function() {
     const scaleIngredientAmountInput = $("#scale-ingredient-amount")[0]
     const scaleIngredientButton = $("#scale-ingredient-button")[0]
     const scaleIngredientSelect = $("#scale-ingredient-select")[0]
+
     scaleIngredientButton.addEventListener("click", function() {
         const name = scaleIngredientSelect.value
         const amount = scaleIngredientAmountInput.value
@@ -90,18 +88,8 @@ $(function() {
         scaleIngredient(name, amount)
     })
 
-    const ingredients = Array.from(ingredientInfos.values()).sort(function(a, b) {
-        if(a.name < b.name)
-            return -1
-        if(a.name > b.name)
-            return 1
-        return 0
-    })
+    const ingredients = Array.from(ingredientInfos.keys()).sort()
     ingredients.forEach(function(k) {
-        let text = k.name
-        if(k.unit) {
-            text += " (" + k.unit + ")"
-        }
-        scaleIngredientSelect.appendChild(new Option(text, k.name))
+        scaleIngredientSelect.appendChild(new Option(k, k))
     })
 })
